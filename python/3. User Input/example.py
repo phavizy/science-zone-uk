790e9303af9eaae7864974e3f2f5803e36a69675
ScienceZoneUK <sciencezoneuk@gmail.com>
2024-08-16 15:12:01 +0100

    Create example.py

---
+++ python/3. User Input/example.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+
+string = input("What is your age? ")
+age = int(string)
+
+rating = int(input("Rate how you're feeling on a scale of 1-10: "))
+
+print("Your name is " + name)
+print("You are " + str(age) + " years old.")
+print("You are feeling like a " + rating + " out of 10.")
