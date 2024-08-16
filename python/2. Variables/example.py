d1af4b8b87ea7d9bc6da17e4174d0738713403f8
ScienceZoneUK <sciencezoneuk@gmail.com>
2024-08-16 14:44:09 +0100

    Create example.py

---
+++ python/2. Variables/example.py
@@ -0,0 +1,16 @@
+number1 = 5
+number2 = 10
+
+result = number1 + number2
+print(result)
+
+result = number1 - number2
+print(result)
+
+result = number1 * number2
+print(result)
+
+result = number1 / number2 # SENSEIS - may return a float
+print(result)
+
+print("Finished calculation! :)")
