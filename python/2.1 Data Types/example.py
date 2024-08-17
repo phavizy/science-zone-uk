da02cc292d3335aed7c89c63066f85051b7504a1
favour <favor.onwuchekwa@gmail.com>
2024-08-17 15:24:54 +0100

    data types

---
+++ python/2.1 Data Types/example.py
@@ -0,0 +1,39 @@
+#python has different data types that can serve different puporses
+'''Some of the common data types are whole numbers which (also know as intergers)
+decimals are known as floats, as string of characters e.g 'bike', '42hudfh9' are
+know as strings and True/False statements are known as boolean data types
+'''
+
+'''here is a brief demo showcasing the assignment of the  data types 6
+listed above into a variable'''
+
+
+#string
+fruit = "apple"
+dessert = 'cake'
+
+#Notice that two different types of quotes were when assign this string variable
+#the string is the only data type that requires quotes when assigning it.
+
+#integer
+num1 =12
+
+#float
+num2 =13.34
+
+#boolean
+state1=True
+state2=False
+
+# if you are not sure of a data type you always use the type() function and print
+#your answer
+
+print(type(num1))
+
+#Chanlenge what data type will num3 below be?
+
+num3 = num1 + num2
+
+
+
+
